9fb6aecf723d1e2d5fd1fb8e0258ca3ef9d45b9a
Tori <viki-tori@hotmail.de>
2023-01-29 21:25:02 +0100

    welcome text

--- NetworkToolkit/app/src/main/java/de/minimaximal/networktoolkit/MainPage.kt
+++ NetworkToolkit/app/src/main/java/de/minimaximal/networktoolkit/MainPage.kt
@@ -7,19 +7,52 @@ import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 
 @Composable
 fun MainScreen(){
         Column {
-            Text("Willkommen im Networktoolkit,")
-            Text("in der Kopfzeile der App sehen Sie die verfügbaren Tools.")
-            Text("Es gibt die \"Ping\" und \"WhoIs\" Funktion.\n")
-            Text("Unter  \"Ping\" kann ein belibiger Host angepingt werden.")
-            Text("Unter \"WhoIs\" können informationen zu einem gewünschten Host ausgelesen werden.")
-            Text("Wir wünschen viel Spaß beim Verwenden unserer App.\n")
-            Text("Bei Fragen stehen wir Ihnen gerne zur Verfügung,")
-            Text("ihr Networktoolkit Team (Maximilian Koch, Paul Antoni, Viktoria Gönnheimer)")
+            Text(
+                buildAnnotatedString {
+                    append("Willkommen im ")
+                    withStyle(style = SpanStyle(Brush.linearGradient(colors = listOf(Color.Red,Color.Blue,Color.Green)), fontWeight = FontWeight.Bold)) {
+                        append("Networktoolkit")
+                    }
+                    append(",\n\nin der Kopfzeile der App sehen Sie die verfügbaren Tools. Es gibt die \"")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Blue)) {
+                        append("Ping")
+                    }
+                    append("\" und \"")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Blue)) {
+                        append("WhoIs")
+                    }
+                    append("\" Funktion. Unter  \"")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Blue)) {
+                        append("Ping")
+                    }
+                    append("\" kann ein beliebiger Host angepingt werden. Unter  \"")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Blue)) {
+                        append("WhoIs")
+                    }
+                    append("\" können Informationen zu einem gewünschten Host ausgelesen werden.")
+                }
+            )
+            Text("Wir wünschen viel Spaß beim Verwenden unserer App, bei Fragen stehen wir Ihnen gerne zur Verfügung.\n")
+            Text(
+                buildAnnotatedString {
+                    append("Ihr ")
+                    withStyle(style = SpanStyle(Brush.linearGradient(colors = listOf(Color.Red,Color.Blue,Color.Green)), fontWeight = FontWeight.Bold)) {
+                        append("Networktoolkit Team")
+                    }
+                    append(" (Maximilian Koch, Paul Antoni, Viktoria Gönnheimer)")
+                }
+            )
         }
 
 }
