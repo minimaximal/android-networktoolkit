cc1782ba06035514a2defef9cfdccd6959374ebf
minimaximal <maximilian.koch@hk.mailbox.de>
2023-01-29 15:30:07 +0100

    ping finished 90%

--- NetworkToolkit/app/src/main/java/de/minimaximal/networktoolkit/pingHost.kt
+++ NetworkToolkit/app/src/main/java/de/minimaximal/networktoolkit/pingHost.kt
@@ -1,5 +1,8 @@
-package de.minimaximal.networktoolkit
+// TODO: instant / faster ui update of result list
+// TODO: scrollbar in result list: vertical, optional horizontal
+// TODO: statistics are not displayed if there is any not working
 
+package de.minimaximal.networktoolkit
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,6 +15,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -20,12 +24,11 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.*
 import java.net.InetAddress
 
-
 @Composable
 fun PingHostView() {
-    var message: String = buildString {
-    }
+    val message = remember { mutableStateOf("") }
     val result = remember { mutableListOf<PingHostResult>() }
+    val statistics = remember { mutableStateOf(PingHostStatistics(null,null,null)) }
     val host = remember { mutableStateOf("hk.de") }
     val timeout = remember { mutableStateOf("5000") }
     val numberOfPings = remember { mutableStateOf("5") }
@@ -64,44 +67,63 @@ fun PingHostView() {
 
 
         Button(onClick = {
+            message.value = ""
+            result.clear()
+            statistics
             try {
                 val timeoutInt = timeout.value.toInt();
                 val numberInt = numberOfPings.value.toInt();
                 val delayLong = delay.value.toLong();
-                pingHost(result, host.value, timeoutInt, numberInt, delayLong)
+                pingHost(result, host.value, timeoutInt, numberInt, delayLong, statistics)
             } catch (e: Exception) {
-                message = "ERROR: please check all input parameters"
+                message.value = "ERROR: please check your input parameters"
             }
 
         }) {
             Text("Ping it")
         }
 
-        if (message.isNotEmpty()) {
-            Text(text = message)
+        if (message.value != "") {
+            Text(text = message.value)
         } else {
-            LazyColumn (verticalArrangement = Arrangement.spacedBy(4.dp)) {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                contentPadding = PaddingValues(horizontal = 12.dp, vertical = 24.dp)
+            )
+            {
                 items(result.size) { index ->
-                    LazyRow (horizontalArrangement = Arrangement.spacedBy(30.dp)) {
+                    LazyRow(horizontalArrangement = Arrangement.spacedBy(30.dp)) {
                         item {
-                            Text(text = result[index].state.toString())
+                            when (result[index].state) {
+                                -1 -> Text(text = (index + 1).toString() + " " + "I/O error, please check network or input")
+                                0 -> Text(text = (index + 1).toString() + " " + host.value + " not reachable")
+                                1 -> Text(text = (index + 1).toString() + " " + host.value + " is reachable")
+                                else -> {
+                                    Text(text = "general error")
+                                }
+                            }
                         }
-                        item{
+                        item {
                             Text(text = result[index].latency.toString() + " ms")
                         }
                     }
                 }
             }
+            if (statistics.value.successfulPings != null && statistics.value.successPercentage != null && statistics.value.averageLatency != null ) {
+                Text(text = statistics.value.successfulPings.toString() + " successful Pings of " + numberOfPings.value + " total Pings (" + statistics.value.successPercentage.toString() + "% success rate)")
+                Text(text = statistics.value.averageLatency.toString() + "ms average latency")
+            }
         }
     }
-    // TODO: switch case for output string
-    // TODO: recycler view ? - clear and instant, lazy,
 }
 
 
-fun pingHost(result: MutableList<PingHostResult>, host: String, timeout: Int, numberOfPings: Int, delay: Long) {
+
+
+
+
+fun pingHost(result: MutableList<PingHostResult>, host: String, timeout: Int, numberOfPings: Int, delay: Long, statistics: MutableState<PingHostStatistics>) {
     CoroutineScope(Dispatchers.Main).launch {
-        result.clear()
         var pings = 0
         while (pings < numberOfPings) {
             try {
@@ -120,7 +142,28 @@ fun pingHost(result: MutableList<PingHostResult>, host: String, timeout: Int, nu
             pings++
             delay(delay)
         }
+        pingHostStatistics(result, numberOfPings, statistics)
     }
+ }
+
+fun pingHostStatistics(result: MutableList<PingHostResult>, numberOfPings: Int, statistics: MutableState<PingHostStatistics>){
+
+    var successfulPings = 0
+    var totalLatency = 0
+
+    result.forEach {
+        if (it.state == 1) {
+            successfulPings++
+            totalLatency += it.latency.toInt()
+        }
+    }
+
+    statistics.value.successfulPings = successfulPings
+    statistics.value.successPercentage = (successfulPings / numberOfPings) * 100
+    statistics.value.averageLatency = totalLatency / successfulPings
 }
 
+
+
 data class PingHostResult (val state: Int, val latency: Long)
+data class PingHostStatistics(var successfulPings: Int?, var successPercentage: Int?, var averageLatency: Int?)
