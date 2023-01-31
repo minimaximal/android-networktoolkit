eec385dd2cc58a6d1f2e56f111a859d89c962c89
Paul Antoni <paul.antoni@siemens-healthineers.com>
2023-01-31 18:47:33 +0100

    BREAKING CHANGE: refactor: move api call to coroutine

--- NetworkToolkit/app/src/main/java/de/minimaximal/networktoolkit/WhoIs.kt
+++ NetworkToolkit/app/src/main/java/de/minimaximal/networktoolkit/WhoIs.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -30,7 +29,7 @@ fun WhoIsView() {
         var text by remember {
             mutableStateOf(TextFieldValue(""))
         }
-        OutlinedTextField(
+        OutlinedTextField(//todo also execute process when pressing enter on keyboard
             value = text,
             label = { Text(text = "Enter address")},
             onValueChange = {address -> text = address}
@@ -74,12 +73,12 @@ fun executeWhoIsQuery(domainName: String): List<String> {
 
     CoroutineScope(Dispatchers.IO).launch{
 
-        val response =
+        val response =//todo maybe catch exception before delivery?
             networkinfo?.getWhoIsInfo("whoisserver/WhoisService?domainName=$domainName&apiKey=at_G0oAXgpjZaZlr7wFw90QeCqyv2dDE&outputFormat=JSON")
-                ?.execute()
+                //?.execute()
 
-        val responseMessage = response?.body()!!
-        result = formatQueryResult(responseMessage)
+        //val responseMessage = response?.body()!!
+        if (response != null) result = formatQueryResult(response)
 
     }
     result = result.ifEmpty { listOf("data") }
@@ -89,7 +88,7 @@ fun executeWhoIsQuery(domainName: String): List<String> {
 interface WhoIsApiInterface{
     @Headers("X-RapidAPI-Key: 50067bc5edmshc4f30e2b88e5c4fp15f033jsn21160563506f", "X-RapidAPI-Host: whoisapi-whois-v2-v1.p.rapidapi.com")
     @GET
-    fun getWhoIsInfo(@Url url: String): Call<WhoisRecord>
+    suspend fun getWhoIsInfo(@Url url: String): WhoisRecord
 }
 
 
